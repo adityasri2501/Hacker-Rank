79b97a47144ce5c1bf3715296bd4fe1b32b90b66
Aditya Kumar Srivastava <adityasrivastava250106@gmail.com>
2026-06-04 16:13:30 +0530

    fixing codde in string formatting

--- solutions/string-formatting.py
+++ solutions/string-formatting.py
@@ -1,7 +1,7 @@
 def formatted(n):
     width = len(bin(n))-2 # max width of one column
     for i in range(1,n+1):
-        print(f"{i:>{width}}{oct(i)[2:]:>{width}}{hex(i)[2:]:>{width}}{bin(i)[2:]:>{width}}") 
+        print(f"{i:>{width}}{oct(i)[2:]:>{width}}{hex(i)[2:].upper():>{width}}{bin(i)[2:]:>{width}}") 
         # or
         # print(f"{i:{width}d} {oct(i)[2:]:>{width}} {hex(i)[2:]:>{width}} {bin(i)[2:]:>{width}}")
         # output is like this 17 0o21 0x11 0b10001
